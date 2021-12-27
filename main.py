c3061b551479d7195298b71613e6dd67ebd59d6f
Jins Mathew <68688737+jinspalakkattu@users.noreply.github.com>
2021-12-27 16:40:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ async def _start(bot: Client, m: Message):
     await AddUserToDatabase(bot, m)
     try:
         await m.reply_text(
-            Config.START_TEXT.format(name=message.from_user.first_name),
+            Config.START_TEXT.format(name=m.from_user.first_name),
             reply_markup=InlineKeyboardMarkup(
                 [
                     [InlineKeyboardButton("Anjumani channel", url="https://t.me/anjumani_zone"), InlineKeyboardButton("Request movies", url="https://t.me/+E-6DtQ6YTjAxNDI1")],
